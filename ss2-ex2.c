0bcc142a11dec0e813853f1d6b57b95c5b24aa46
BMVu2403 <buiminhvu06hn@gmail.com>
2024-11-20 19:19:33 +0700

    Add files via upload

---
+++ ss2-ex2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;   
+    int b = 20;   
+    int sum;      
+
+    sum = a + b;  
+
+    printf("Tong cua a va b la: %d\n", sum);  
+    return 0;
+}
+
