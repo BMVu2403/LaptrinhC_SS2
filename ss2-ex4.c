8cbc0205a9efda4db65f00d9cdab26e79c341ef8
BMVu2403 <buiminhvu06hn@gmail.com>
2024-11-20 19:33:19 +0700

    Add files via upload

---
+++ ss2-ex4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int side = 7;  
+    int perimeter, area;  
+
+    perimeter = 4 * side;
+
+    area = side * side;
+
+    printf("Chu vi hinh vuong la: %d\n", perimeter);
+    printf("Dien tich hinh vuong la: %d\n", area);
+
+    return 0;
+}
+
